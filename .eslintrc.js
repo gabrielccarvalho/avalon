18a26fc7af31815372a815b1675c2614be92129d
gabrielccarvalho <caampos22@gmail.com>
2020-07-12 12:59:32 -0300

    chore: Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,29 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  rules: {
-  }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    ignorePatterns: ['build', 'node_modules'],
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        'linebreak-style': ['error', 'unix'],
+    },
 };
